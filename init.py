54010f2d9fc75deb9f83e50eda5f04856ea3721a
Sander Hellesø <sanderhelleso@hotmail.com>
2020-05-06 13:13:19 +0200

    Update init.py

--- init.py
+++ init.py
@@ -1,14 +1,35 @@
 import subprocess
 import time
+import sys
 
-MOVE_DUR = 1
+MOVE_DUR = 3
+initial_dir = sys.argv[1]
 
 print("running move.py")
 
-script = ['python', 'move.py', '0']
-subprocess = subprocess.Popen(script)
 
-time.sleep(MOVE_DUR)
-subprocess.terminate()
+def run_script(direction):
+    script = ['python', 'move.py', direction]
+    process = subprocess.Popen(script)
 
+    delay = direction != "0"
+    clear(process, delay)
+
+
+def clear(process, delay):
+    if delay:
+        time.sleep(MOVE_DUR)
+
+    process.terminate()
+
+
+def start():
+    run_script(initial_dir)
+
+    # stop movement after each direction passed
+    if not initial_dir == "0":
+        run_script("0")
+
+
+start()
 print("Done running move.py")
