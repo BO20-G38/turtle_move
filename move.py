40c293adfbb3ff876da18b1f7d9460b294345828
Sander Hellesø <sanderhelleso@hotmail.com>
2020-05-06 13:12:49 +0200

    Updated move.py with new script features

--- move.py
+++ move.py
@@ -1,35 +1,55 @@
-import subprocess
-import time
+import os
 import sys
 
-MOVE_DUR = 3
-initial_dir = sys.argv[1]
-
-print("running move.py")
+direction = int(sys.argv[1])
+BASE_CMD = "rostopic pub /cmd_vel geometry_msgs/Twist -- "
 
 
-def run_script(direction):
-    script = ['python', 'move.py', direction]
-    process = subprocess.Popen(script)
-
-    delay = direction != "0"
-    clear(process, delay)
+def stop():
+    linear = [0, 0, 0]
+    angular = [0, 0, 0]
+    return stringify(linear) + " " + stringify(angular)
 
 
-def clear(process, delay):
-    if delay:
-        time.sleep(MOVE_DUR)
-
-    process.terminate()
-
-
-def start():
-    run_script(initial_dir)
-
-    # stop movement after each direction passed
-    if not initial_dir == "0":
-        run_script("0")
+def left():
+    linear = [1, 0, 0]
+    angular = [0, 0, -5]
+    return stringify(linear) + " " + stringify(angular)
 
 
-start()
-print("Done running move.py")
+def right():
+    linear = [1, 0, 0]
+    angular = [0, 0, 5]
+    return stringify(linear) + " " + stringify(angular)
+
+
+def forward():
+    linear = [2, 0, 0]
+    angular = [0, 0, 0]
+    return stringify(linear) + " " + stringify(angular)
+
+
+def backward():
+    linear = [-2, 0, 0]
+    angular = [0, 0, 0]
+    return stringify(linear) + " " + stringify(angular)
+
+
+def stringify(arr):
+    return "'" + str(arr) + "'"
+
+
+def move(movement_type):
+    switch = {
+        0: stop(),
+        1: right(),
+        2: left(),
+        3: forward(),
+        4: backward()
+    }
+
+    coordinate = switch.get(movement_type)
+    os.system(BASE_CMD + coordinate)
+
+
+move(direction)
